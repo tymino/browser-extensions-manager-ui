b64d3497a2106ea427f221305ca4623fc0e1b7c0
tymino <d_team-lex@bk.ru>
2025-06-02 19:41:09 +0300

    button theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import Link from './components/Link'
 import useTheme from './hooks/useTheme'
+
+import Link from './components/Link'
+import ButtonTheme from './components/ButtonTheme'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
+
   return (
     <div className="h-screen bg-linear-(--bg-light-gradient) dark:bg-linear-(--bg-dark-gradient) ">
-      <button
-        className="w-full h-fulls p-3.5 bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded"
-        onClick={toggleTheme}
-      >
-        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
-      </button>
+      <ButtonTheme theme={theme} toggleTheme={toggleTheme} />
 
       <div className="text-primary-200 dark:text-primary-800">Hello React</div>
       <Link href="#" name="click link" />
---
+++ src/components/ButtonTheme.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+interface IProps {
+  theme: string
+  toggleTheme: () => void
+}
+
+const svgSun = (
+  <path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.98"
+    d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z"
+  />
+)
+
+const svgMoon = (
+  <path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.98"
+    d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z"
+  />
+)
+
+const ButtonTheme: React.FC<IProps> = ({ theme, toggleTheme }) => {
+  return (
+    <button
+      className="p-2 bg-primary-100 text-primary-800 dark:bg-primary-800 dark:text-primary-0 rounded-lg"
+      onClick={toggleTheme}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22">
+        <g clip-path="url(#a)">{theme === 'light' ? svgMoon : svgSun}</g>
+        <defs>
+          <clipPath id="a">
+            <path fill="#fff" d="M0 0h22v22H0z" />
+          </clipPath>
+        </defs>
+      </svg>
+    </button>
+  )
+}
+export default ButtonTheme
