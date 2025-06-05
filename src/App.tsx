b3d093571c73f10667d5e55d61c4876eb3dad91f
tymino <d_team-lex@bk.ru>
2025-06-05 19:55:55 +0300

    main page style template

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import useTheme from './hooks/useTheme'
 
-import Link from './components/Link'
 import ButtonTheme from './components/ButtonTheme'
 import Logo from './components/Logo'
 
@@ -9,12 +8,37 @@ const App = () => {
 
   return (
     <div className="h-screen bg-linear-(--bg-light-gradient) dark:bg-linear-(--bg-dark-gradient) ">
-      <ButtonTheme theme={theme} toggleTheme={toggleTheme} />
+      <div className="flex flex-col max-w-7xl h-full p-4 mx-auto">
+        <div className="flex justify-between">
+          <Logo />
+          <ButtonTheme theme={theme} toggleTheme={toggleTheme} />
+        </div>
 
-      <Logo />
+        <div className="flex justify-between">
+          <p>extensions list</p>
 
-      <div className="text-primary-200 dark:text-primary-800">Hello React</div>
-      <Link href="#" name="click link" />
+          <div>
+            <button>all</button>
+            <button>active</button>
+            <button>inactive</button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 grid-rows-4 gap-4">
+          <div className="bg-amber-600">a</div>
+          <div className="bg-blue-500">a</div>
+          <div className="bg-amber-600">a</div>
+          <div className="bg-blue-500">a</div>
+          <div className="bg-amber-600">a</div>
+          <div className="bg-blue-500">a</div>
+          <div className="bg-amber-600">a</div>
+          <div className="bg-blue-500">a</div>
+          <div className="bg-amber-600">a</div>
+          <div className="bg-blue-500">a</div>
+          <div className="bg-amber-600">a</div>
+          <div className="bg-blue-500">a</div>
+        </div>
+      </div>
     </div>
   )
 }
